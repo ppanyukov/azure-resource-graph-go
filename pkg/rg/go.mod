6ae6f63bf8acaaa3475f374489ee0cb615d0d2a4
Philip Panyukov <ppanyukov@googlemail.com>
2026-09-06 14:51:54 +0300

    Bump minimum Go version to 1.23 (#17)

--- pkg/rg/go.mod
+++ pkg/rg/go.mod
@@ -1,6 +1,6 @@
 module github.com/ppanyukov/azure-resource-graph-go/pkg/rg
 
-go 1.18
+go 1.23
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.3.1
--- pkg/rg/rg.go
+++ pkg/rg/rg.go
@@ -1,6 +1,3 @@
-//go:build go1.18
-// +build go1.18
-
 // Package rg provides a simple interface to run Azure Resource Graph queries
 // and unmarshall results into custom types. It is based on the modified official
 // Azure SDK for Go.
